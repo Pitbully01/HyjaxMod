f71794e1171f0a279ddb0791623327903ab0ea17
Pitbully01 <Pitgully@hotmail.com>
2020-12-05 23:38:44 +0100

    dooing stuff

--- src/main/java/de/hyjax/hyjaxmod/bases/blocks/ButtonBlock.java
+++ src/main/java/de/hyjax/hyjaxmod/bases/blocks/ButtonBlock.java
@@ -1,7 +1,6 @@
 package de.hyjax.hyjaxmod.bases.blocks;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.WoodButtonBlock;
 import net.minecraft.block.material.Material;
--- src/main/java/de/hyjax/hyjaxmod/bases/blocks/StairsBlock.java
+++ src/main/java/de/hyjax/hyjaxmod/bases/blocks/StairsBlock.java
@@ -1,7 +1,6 @@
 package de.hyjax.hyjaxmod.bases.blocks;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraftforge.common.ToolType;
